ec0b0d08e0322dee156bcbfa4a79faac332f929b
Kaijoh <johnkailgwapo1232@gmail.com>
2023-06-19 12:48:07 +0800

    make it better again6

--- mygame/settings.py
+++ mygame/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
